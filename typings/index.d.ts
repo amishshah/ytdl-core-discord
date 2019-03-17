6a394f14d7907668c05650b833f092704e7da4b9
Jeroen Claassens <jeroen.claassens@live.nl>
2019-03-17 22:26:01 +0100

    Fix linting and exports in typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,6 @@
 import { Readable } from 'stream';
 import { downloadOptions, videoFormat } from 'ytdl-core';
 export interface PrismVideoFormat extends videoFormat {
-    audio_sample_rate?: number;
+	audio_sample_rate?: number;
 }
 export declare function download(url: string, options?: downloadOptions): Promise<Readable>;
-export default download;
