a1bff705094a9ef8b3a1ec02f9b45381ac478cbf
Amish Shah <amishshah.2k@gmail.com>
2020-09-11 09:40:46 +0100

    deps: update ytdl-core to latest version

--- index.js
+++ index.js
@@ -22,44 +22,40 @@ function nextBestFormat(formats, isLive) {
 	return formats.find(format => !format.bitrate) || formats[0];
 }
 
-function download(url, options = {}) {
-	return new Promise((resolve, reject) => {
-		ytdl.getInfo(url, (err, info) => {
-			if (err) return reject(err);
-			// Prefer opus
-			const format = info.formats.find(filter);
-			const canDemux = format && info.length_seconds != 0;
-			if (canDemux) options = { ...options, filter };
-			else if (info.length_seconds != 0) options = { ...options, filter: 'audioonly' };
-			if (canDemux) {
-				const demuxer = new prism.opus.WebmDemuxer();
-				return resolve(ytdl.downloadFromInfo(info, options).pipe(demuxer).on('end', () => demuxer.destroy()));
-			} else {
-				const bestFormat = nextBestFormat(info.formats, info.player_response.videoDetails.isLiveContent);
-				if (!bestFormat) return reject('No suitable format found');
-				const transcoder = new prism.FFmpeg({
-					args: [
-						'-reconnect', '1',
-						'-reconnect_streamed', '1',
-						'-reconnect_delay_max', '5',
-						'-i', bestFormat.url,
-						'-analyzeduration', '0',
-						'-loglevel', '0',
-						'-f', 's16le',
-						'-ar', '48000',
-						'-ac', '2',
-					],
-				});
-				const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
-				const stream = transcoder.pipe(opus);
-				stream.on('close', () => {
-					transcoder.destroy();
-					opus.destroy();
-				});
-				return resolve(stream);
-			}
+async function download(url, options = {}) {
+	const info = await ytdl.getInfo(url);
+	// Prefer opus
+	const format = info.formats.find(filter);
+	const canDemux = format && info.length_seconds != 0;
+	if (canDemux) options = { ...options, filter };
+	else if (info.length_seconds != 0) options = { ...options, filter: 'audioonly' };
+	if (canDemux) {
+		const demuxer = new prism.opus.WebmDemuxer();
+		return ytdl.downloadFromInfo(info, options).pipe(demuxer).on('end', () => demuxer.destroy());
+	} else {
+		const bestFormat = nextBestFormat(info.formats, info.player_response.videoDetails.isLiveContent);
+		if (!bestFormat) throw new Error('No suitable format found');
+		const transcoder = new prism.FFmpeg({
+			args: [
+				'-reconnect', '1',
+				'-reconnect_streamed', '1',
+				'-reconnect_delay_max', '5',
+				'-i', bestFormat.url,
+				'-analyzeduration', '0',
+				'-loglevel', '0',
+				'-f', 's16le',
+				'-ar', '48000',
+				'-ac', '2',
+			],
 		});
-	});
+		const opus = new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 });
+		const stream = transcoder.pipe(opus);
+		stream.on('close', () => {
+			transcoder.destroy();
+			opus.destroy();
+		});
+		return stream;
+	}
 }
 
 module.exports = Object.assign(download, ytdl);
